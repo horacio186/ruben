58a4d5bc4fe5a151c4c80807b35643127714ca37
horacio <ortega.farina@gmail.com>
2020-05-10 13:44:33 -0400

    otros otros cambios

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+
+    var menuBtn = $('.menu-icon');
+    var menu = $('.navigation ul');
+    menuBtn.addClass("show");
+  
+ /*   if(menu.hasClass('show')) {
+        menu.removeClass('show');
+    } else {*/
+        menu.addClass('show');
+  //  }
+  
+})
+
+   
+
